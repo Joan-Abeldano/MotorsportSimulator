7e66965e10a2e7c9e564a47fb95353db3174e50c
Joan-Abeldano <joanmabeldano@gmail.com>
2024-12-11 13:04:50 -0600

    Championship class attributes

--- src/Model/Championship.java
+++ src/Model/Championship.java
@@ -4,11 +4,24 @@ import java.util.ArrayList;
 
 public class Championship {
     private String championshipName;
+    private ArrayList<Event> championshipEvents;
+    private ArrayList<GrandPrix> championshipGPs;
     private ArrayList<Team> championshipTeams;
-    private ArrayList<Race> championshipRaces;
-    private ArrayList<Qualy> championshipQualifyings;
-    private ArrayList<Practice> championshipPractices;
     private ArrayList<Integer> pointSystem;
     //Championship rules
-    
+    private int fastestLapPoints;
+    private int maxDriverPerTeam;
+    private int minDriversPerTeam;
+    private boolean weatherEnabled;
+    private int minPitStops;
+    private boolean carUpgradesEnabled;
+    private boolean safetyCarEnabled;
+    private boolean virtualSafetyCarEnabled;
+    private boolean redFlagEnabled;
+
+    public Championship(String championshipName) {
+        this.championshipName = championshipName;
+    }
+
+
 }
